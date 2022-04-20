771a30858d24a82fe5513252da4e5aef76bbbba4
Irish Moore <jasonamoore@frontier.com>
2022-04-20 13:36:30 -0400

    coded new queries

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { authMiddleware } = require('./utils/auth');
 
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -12,10 +13,10 @@ const app = express();
 
 const startServer = async () => {
   // create a new Apollo server and pass in our schema data
-  const server = new ApolloServer({ 
+  const server = new ApolloServer({
     typeDefs, 
     resolvers, 
-    // context: authMiddleware 
+    context: authMiddleware
   });
 
   // Start the Apollo server
